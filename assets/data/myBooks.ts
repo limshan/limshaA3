efcd4d35efa5aafc833bf25a83968d10b143b8af
limshan <52501823+limshan@users.noreply.github.com>
2019-07-24 10:26:35 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import {myBookClass} from '../../app/bookInfo';
+
+
+export const MyBooksArray: myBookClass[] = 
+[
+  {bookName:'Harry Potter', bookAuthor: 'J.K. Rowling', bookGenre:'Fantasy', yearPublished:1997, picture:'assets/images/book_harryPotter.jpg'},
+  {bookName:'Anne of Green Gables', bookAuthor: 'L.M. Montgomery', bookGenre:'Canadian Novel', yearPublished:1908, picture:'assets/images/Book_AnneGreenGables.jpg'},
+  {bookName:'How to be a Straight A Student', bookAuthor: 'Cal Newport', bookGenre:'Study Guide', yearPublished:2006, picture:'assets/images/Book_StraightA.jpg'},
+  {bookName:'To Kill a Mockingbird', bookAuthor: 'Harper Lee', bookGenre:'Fiction', yearPublished:1960, picture:'assets/images/Book_ToKillAMockingbird.jpg'}
+]
